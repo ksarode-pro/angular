24ace71819e7c5e05a12d10e726ca090ec78dc62
Kiran Sarode <ksarode.in@outlook.com>
2025-03-20 19:23:39 +0530

    task completion added

--- sample_app/src/app/task/task-list/task-list.component.ts
+++ sample_app/src/app/task/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
 import { Task } from '../../common/custom_types';
 
@@ -8,7 +8,13 @@ import { Task } from '../../common/custom_types';
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
+
 export class TaskListComponent {
   @Input({ required: true }) task!: Task;
+  @Output() finish = new EventEmitter();
+
+  finishTask() {
+    this.finish.emit(this.task);
+  }
 }
 
